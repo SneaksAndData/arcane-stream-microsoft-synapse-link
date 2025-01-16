8668468d36f711e7e2bfa078b97b17fe804145ad
Vitalii Savitskii <visa@ecco.com>
2025-01-16 11:07:13 +0100

    Add batch size logging

--- src/main/scala/services/streaming/processors/MergeBatchProcessor.scala
+++ src/main/scala/services/streaming/processors/MergeBatchProcessor.scala
@@ -21,8 +21,14 @@ class MergeBatchProcessor(jdbcConsumer: JdbcConsumer[StagedVersionedBatch])
    * @return ZPipeline (stream source for the stream graph).
    */
   override def process: ZPipeline[Any, Throwable, StagedVersionedBatch, StagedVersionedBatch] =
-    ZPipeline.mapZIO(batch => ZIO.fromFuture(implicit ec => jdbcConsumer.applyBatch(batch)).map(_ => batch))
+    ZPipeline
+      .mapZIO(logBatchSize)
+      .mapZIO(batch => ZIO.fromFuture(implicit ec => jdbcConsumer.applyBatch(batch)).map(_ => batch))
 
+
+  private def logBatchSize: ZIO[Any, Nothing, StagedVersionedBatch] =
+    for _ <- ZIO.log.info(s"Processing batch with ${batch.rows.size} rows")
+    yield batch
 
 object MergeBatchProcessor:
 
