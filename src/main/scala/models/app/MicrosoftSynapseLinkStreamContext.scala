ed303261c41402aac8f7736657d450821083ee87
Vitalii Savitskii <visa@ecco.com>
2025-01-16 10:37:32 +0100

    Stream settings refactoring (#8)
    * Stream settings refactoring
    
    * Fix
    
    * Still need to build the debug image
    
    * Also bypass the staging table name prefix
    
    * Fix CRD
    
    * Not use debug image
    
    * Fix log severity

--- src/main/scala/models/app/MicrosoftSynapseLinkStreamContext.scala
+++ src/main/scala/models/app/MicrosoftSynapseLinkStreamContext.scala
@@ -1,14 +1,15 @@
 package com.sneaksanddata.arcane.microsoft_synapse_link
 package models.app
 
+import models.app.contracts.StreamSpec
+
 import com.sneaksanddata.arcane.framework.models.app.StreamContext
-import com.sneaksanddata.arcane.framework.models.settings.{GroupingSettings, SinkSettings, VersionedDataGraphBuilderSettings}
+import com.sneaksanddata.arcane.framework.models.settings.{GroupingSettings, VersionedDataGraphBuilderSettings}
 import com.sneaksanddata.arcane.framework.services.cdm.CdmTableSettings
 import com.sneaksanddata.arcane.framework.services.consumers.JdbcConsumerOptions
-import com.sneaksanddata.arcane.framework.services.lakehouse.{IcebergCatalogCredential, S3CatalogFileIO}
 import com.sneaksanddata.arcane.framework.services.lakehouse.base.IcebergCatalogSettings
+import com.sneaksanddata.arcane.framework.services.lakehouse.{IcebergCatalogCredential, S3CatalogFileIO}
 import zio.ZLayer
-import upickle.default.*
 
 import java.time.Duration
 
@@ -18,63 +19,15 @@ trait AzureConnectionSettings:
   val account: String
   val accessKey: String
 
-/**
- * The configuration of Iceberg sink.
- */
-case class CatalogSettings(namespace: String, warehouse: String, catalogUri: String) derives ReadWriter
-
-
-/**
- * The specification for the stream.
- *
- * @param name                         The name of the CDM table
- * @param baseLocation                 The entity base location
- * @param rowsPerGroup                 The number of rows per group in the staging table
- * @param groupingIntervalSeconds      The grouping interval in seconds
- * @param groupsPerFile                The number of groups per file
- * @param lookBackInterval             The look back interval in seconds
- * @param changeCaptureIntervalSeconds The change capture interval in seconds
- * @param partitionExpression          Partition expression for partitioning the data in the staging table (optional)
- */
-case class StreamSpec(name: String,
-                      baseLocation: String,
-
-                      // Grouping settings
-                      rowsPerGroup: Int,
-                      groupingIntervalSeconds: Int,
-                      groupsPerFile: Int,
-                      lookBackInterval: Int,
-
-                      // Timeouts
-                      changeCaptureIntervalSeconds: Int,
-
-                      // Iceberg settings
-                      catalog: CatalogSettings,
-
-                      stagingLocation: Option[String],
-                      sinkLocation: String,
-                     
-                      targetTableName: String,
-                      archiveTableName: String,
-                     
-                      partitionExpression: Option[String])
-  derives ReadWriter
-
 trait TargetTableSettings:
   val targetTableFullName: String
 
 trait ArchiveTableSettings:
   val archiveTableFullName: String
 
-// TODO
-trait StagingTableSettings:
-  val tableNamePrefix: String
-  val namespace: String
-  val warehouse: String
-  val catalogUri: String
-
 /**
  * The context for the SQL Server Change Tracking stream.
+ *
  * @param spec The stream specification
  */
 case class MicrosoftSynapseLinkStreamContext(spec: StreamSpec) extends StreamContext
@@ -88,31 +41,31 @@ case class MicrosoftSynapseLinkStreamContext(spec: StreamSpec) extends StreamCon
 
   override val rowsPerGroup: Int = spec.rowsPerGroup
   override val lookBackInterval: Duration = Duration.ofSeconds(spec.lookBackInterval)
-  override val changeCaptureInterval: Duration = Duration.ofSeconds(spec.changeCaptureIntervalSeconds)
+  override val changeCaptureInterval: Duration = Duration.ofSeconds(spec.sourceSettings.changeCaptureIntervalSeconds)
   override val groupingInterval: Duration = Duration.ofSeconds(spec.groupingIntervalSeconds)
 
-  override val namespace: String = spec.catalog.namespace
-  override val warehouse: String = spec.catalog.warehouse
-  override val catalogUri: String = spec.catalog.catalogUri
+  override val namespace: String = spec.stagingDataSettings.catalog.namespace
+  override val warehouse: String = spec.stagingDataSettings.catalog.warehouse
+  override val catalogUri: String = spec.stagingDataSettings.catalog.catalogUri
+  override val stagingLocation: Option[String] = spec.stagingDataSettings.dataLocation
 
   override val additionalProperties: Map[String, String] = IcebergCatalogCredential.oAuth2Properties
   override val s3CatalogFileIO: S3CatalogFileIO = S3CatalogFileIO
 
-  override val stagingLocation: Option[String] = spec.stagingLocation
-
   override val connectionUrl: String = sys.env("ARCANE_FRAMEWORK__MERGE_SERVICE_CONNECTION_URI")
 
-  override val targetTableFullName: String = spec.targetTableName
-  override val archiveTableFullName: String = spec.archiveTableName
+  override val targetTableFullName: String = spec.sinkSettings.targetTableName
+  override val archiveTableFullName: String = spec.sinkSettings.archiveTableName
 
   override val endpoint: String = sys.env("ARCANE_FRAMEWORK__STORAGE_ENDPOINT")
   override val container: String = sys.env("ARCANE_FRAMEWORK__STORAGE_CONTAINER")
   override val account: String = sys.env("ARCANE_FRAMEWORK__STORAGE_ACCOUNT")
   override val accessKey: String = sys.env("ARCANE_FRAMEWORK__STORAGE_ACCESS_KEY")
 
+  val stagingTableNamePrefix: String = spec.stagingDataSettings.tableNamePrefix
 
-given Conversion[MicrosoftSynapseLinkStreamContext, CdmTableSettings] with
-  def apply(context: MicrosoftSynapseLinkStreamContext): CdmTableSettings = CdmTableSettings(context.spec.name, context.spec.baseLocation)
+given Conversion[StreamSpec, CdmTableSettings] with
+  def apply(spec: StreamSpec): CdmTableSettings = CdmTableSettings(spec.sourceSettings.name, spec.sourceSettings.baseLocation)
 
 object MicrosoftSynapseLinkStreamContext {
   type Environment = StreamContext
@@ -125,16 +78,13 @@ object MicrosoftSynapseLinkStreamContext {
     & AzureConnectionSettings
     & ArchiveTableSettings
     & TargetTableSettings
+    & MicrosoftSynapseLinkStreamContext
 
   /**
    * The ZLayer that creates the VersionedDataGraphBuilder.
    */
-  val layer: ZLayer[Any, Throwable, Environment] =
-    sys.env.get("STREAMCONTEXT__SPEC") map { raw =>
-      val spec = read[StreamSpec](raw)
-      val context = MicrosoftSynapseLinkStreamContext(spec)
-      ZLayer.succeed(context) ++ ZLayer.succeed[CdmTableSettings](context)
-    } getOrElse {
-      ZLayer.fail(new Exception("The stream context is not specified."))
-    }
+  val layer: ZLayer[Any, Throwable, Environment] = StreamSpec
+      .fromEnvironment("STREAMCONTEXT__SPEC")
+      .map(c => ZLayer.succeed(MicrosoftSynapseLinkStreamContext(c)) ++ ZLayer.succeed[CdmTableSettings](c))
+      .getOrElse(ZLayer.fail(new Exception("The stream context is not specified.")))
 }
---
+++ src/main/scala/models/app/contracts/StreamSpec.scala
@@ -0,0 +1,56 @@
+package com.sneaksanddata.arcane.microsoft_synapse_link
+package models.app.contracts
+
+import upickle.default.*
+
+/**
+ * The configuration of Iceberg catalog
+ */
+case class CatalogSettings(namespace: String, warehouse: String, catalogUri: String) derives ReadWriter
+
+/**
+ * The configuration of staging data.
+ */
+case class StagingDataSettings(tableNamePrefix: String, catalog: CatalogSettings, dataLocation: Option[String]) derives ReadWriter
+
+/**
+ * The configuration of Iceberg sink.
+ */
+case class SinkSettings(targetTableName: String, archiveTableName: String) derives ReadWriter
+
+
+/**
+ * The configuration of the stream source.
+ */
+case class SourceSettings(name: String, baseLocation: String, changeCaptureIntervalSeconds: Int) derives ReadWriter
+
+/**
+ * The specification for the stream.
+ *
+ * @param name                         The name of the CDM table
+ * @param baseLocation                 The entity base location
+ * @param rowsPerGroup                 The number of rows per group in the staging table
+ * @param groupingIntervalSeconds      The grouping interval in seconds
+ * @param groupsPerFile                The number of groups per file
+ * @param lookBackInterval             The look back interval in seconds
+ * @param partitionExpression          Partition expression for partitioning the data in the staging table (optional)
+ */
+case class StreamSpec(sourceSettings: SourceSettings,
+
+                      // Grouping settings
+                      rowsPerGroup: Int,
+                      groupingIntervalSeconds: Int,
+                      groupsPerFile: Int,
+                      lookBackInterval: Int,
+
+                      // Iceberg settings
+                      stagingDataSettings: StagingDataSettings,
+                      sinkSettings: SinkSettings,
+
+                      partitionExpression: Option[String])
+  derives ReadWriter
+
+object StreamSpec:
+
+  def fromEnvironment(envVarName: String): Option[StreamSpec] =
+    sys.env.get(envVarName).map(env => read[StreamSpec](env))
