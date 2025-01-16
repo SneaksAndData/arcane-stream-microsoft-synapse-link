664c754075729de863e6f39bb2d690e7ca43f920
Vitalii Savitskii <visa@ecco.com>
2025-01-16 12:10:50 +0100

    Fix the timestamp conversions

--- src/main/scala/services/streaming/processors/TypeAlignmentService.scala
+++ src/main/scala/services/streaming/processors/TypeAlignmentService.scala
@@ -10,6 +10,8 @@ import zio.{Chunk, ZIO, ZLayer}
 import zio.Chunk
 import zio.stream.ZPipeline
 
+import java.time.format.DateTimeFormatter
+
 /**
  * The service that processes a DataRow and aligns the types of the cells.
  * This operation is necessary to ensure that the data is correctly processed by the stream graph.
@@ -22,21 +24,21 @@ trait TypeAlignmentService:
 private class TypeAlignmentServiceImpl extends TypeAlignmentService:
 
   override def alignTypes(row: DataRow): DataRow = row map { cell =>
-    DataCell(cell.name, cell.Type, convertType(cell.Type, cell.value))
+    DataCell(cell.name, cell.Type, convertType(cell.name, cell.Type, cell.value))
   }
 
-  private def convertType(arcaneType: ArcaneType, value: Any): Any =
+  private def convertType(cellName: String, arcaneType: ArcaneType, value: Any): Any =
     value match
       case None => null
-      case Some(v) => convertSome(arcaneType, v)
+      case Some(v) => convertSome(cellName, arcaneType, v)
 
-  private def convertSome(arcaneType: ArcaneType, value: Any): Any = arcaneType match
+  private def convertSome(cellName: String, arcaneType: ArcaneType, value: Any): Any = arcaneType match
     case LongType => value.toString.toLong
     case ByteArrayType => value.toString.getBytes
     case BooleanType => value.toString.toBoolean
     case StringType => value.toString
     case DateType => java.sql.Date.valueOf(value.toString)
-    case TimestampType => null //java.sql.Timestamp.valueOf(value.toString) // TODO
+    case TimestampType => convertToTimeStamp(cellName, value)
     case DateTimeOffsetType => java.time.OffsetDateTime.parse(value.toString)
     case BigDecimalType => BigDecimal(value.toString)
     case DoubleType => value.toString.toDouble
@@ -45,7 +47,21 @@ private class TypeAlignmentServiceImpl extends TypeAlignmentService:
     case ShortType => value.toString.toShort
     case TimeType => java.sql.Time.valueOf(value.toString)
 
-
+private def convertToTimeStamp(value: Any, columnName: String): java.sql.Timestamp = value match
+  case v: String =>
+    columnName match
+      case "SinkCreatedOn" | "SinkModifiedOn" =>
+        val formatter = DateTimeFormatter.ofPattern("M/d/yyyy h:mm:ss a")
+        val localDateTime = LocalDateTime.parse(dateTimeString, formatter)
+        val timestamp = Timestamp.valueOf(localDateTime)
+      case "CreatedOn" =>
+        val localDateTime = LocalDateTime.parse(dateTimeString, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+        val timestamp = Timestamp.valueOf(localDateTime)
+      case _ =>
+        val localDateTime = LocalDateTime.parse(dateTimeString, DateTimeFormatter.ISO_INSTANT)
+        val timestamp = Timestamp.valueOf(localDateTime)
+  case _ => throw new IllegalArgumentException(s"Invalid timestamp type: ${value.getClass}")
+  
 object TypeAlignmentService:
   val layer: ZLayer[Any, Nothing, TypeAlignmentService] = ZLayer.succeed(new TypeAlignmentServiceImpl)
 
