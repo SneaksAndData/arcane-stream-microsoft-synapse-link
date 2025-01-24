d80f8ec6bd74781ea5e06038cf3bcf33c5350bd6
Vitalii Savitskii <visa@ecco.com>
2025-01-24 13:28:27 +0100

    Fix unit tests

--- src/test/scala/TypeAlignmentServiceTests.scala
+++ src/test/scala/TypeAlignmentServiceTests.scala
@@ -14,18 +14,18 @@ import java.time.LocalDateTime
 class TypeAlignmentServiceTests extends AnyFlatSpec with Matchers {
 
   private val validTimeStamps = Table(
-    ("line", "result"),
-    (List(DataCell("timestamp", ArcaneType.TimestampType, Some("2020-01-01T00:00:00.000Z"))), LocalDateTime.now),
-    (List(DataCell("timestamp", ArcaneType.TimestampType, Some("1900-01-01T00:00:00.0000000"))), LocalDateTime.now),
-    (List(DataCell("timestamp", ArcaneType.DateTimeOffsetType, Some("0001-01-03T00:00:00.0000000"))), LocalDateTime.now),
-    (List(DataCell("timestamp", ArcaneType.DateTimeOffsetType, Some("2023-08-15T02:45:20.0000000+00:00"))), LocalDateTime.now),
+    "line",
+    List(DataCell("timestamp", ArcaneType.TimestampType, Some("2020-01-01T00:00:00.000Z"))),
+    List(DataCell("timestamp", ArcaneType.TimestampType, Some("1900-01-01T00:00:00.0000000"))),
+    List(DataCell("timestamp", ArcaneType.DateTimeOffsetType, Some("0001-01-03T00:00:00.0000000"))),
+    List(DataCell("timestamp", ArcaneType.DateTimeOffsetType, Some("2023-08-15T02:45:20.0000000+00:00"))),
   )
 
 
   it should "Should parse valid timestamps" in {
     val tas = TypeAlignmentServiceImpl()
-    forAll (validTimeStamps) { (line, result) =>
-      tas.alignTypes(line) //should equal(result)
+    forAll (validTimeStamps) { line =>
+      noException should be thrownBy tas.alignTypes(line)
     }
   }
 
