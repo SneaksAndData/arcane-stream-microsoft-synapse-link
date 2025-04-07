89229574341dc9dbb22a6a022f231214ea12ae44
George Zubrienko <zubrienkog@gmail.com>
2025-04-07 11:20:32 +0200

    Refactor to v1 (#123)

--- src/main/scala/models/app/streaming/SourceCleanupRequest.scala
+++
@@ -1,8 +0,0 @@
-package com.sneaksanddata.arcane.microsoft_synapse_link
-package models.app.streaming
-
-import com.sneaksanddata.arcane.framework.services.storage.models.azure.AdlsStoragePath
-
-case class SourceCleanupRequest(prefix: AdlsStoragePath)
-case class SourceCleanupResult(blobName: AdlsStoragePath, deleteMarker: AdlsStoragePath)
-case class SourceDeletionResult(blobName: AdlsStoragePath, succeed: Boolean)
